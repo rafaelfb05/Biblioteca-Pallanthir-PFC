224b4cc4b04ba1a34d3d5df1ebcb9ad2d9345dab
Rafael Fernandes Barbosa <rafafb30@gmail.com>
2026-09-08 19:22:04 -0300

    teste de conexão com api

---
+++ src/main/java/br/com/pfc/biblioteca/main/Main.java
@@ -0,0 +1,46 @@
+package br.com.pfc.biblioteca.main;
+
+import br.com.pfc.biblioteca.dto.DadosLivro;
+import br.com.pfc.biblioteca.service.ConsumoApi;
+import br.com.pfc.biblioteca.service.ConverteDados;
+
+import java.util.Scanner;
+
+public class Main {
+      private Scanner scanner = new Scanner(System.in);
+      private ConsumoApi consumo = new ConsumoApi();
+      private ConverteDados conversor = new ConverteDados();
+      private final String ENDERECO = "https://www.googleapis.com/books/v1/volumes?q=";
+      private final String API_KEY = System.getenv("BOOK_API");
+
+      public void exibeMenu() {
+            int opcao = -1;
+            String mensagem = """
+                    --------BEM VINDO AO PALLANTHIR---------
+                    
+                    digite:
+                    -1 para pesquisar um livro
+                    """;
+
+
+
+            while(opcao != 0){
+                  System.out.println(mensagem);
+                  opcao = scanner.nextInt();
+                  scanner.nextLine();
+
+                  switch (opcao) {
+                        case 1:
+
+                  }
+            }
+      }
+      private DadosLivro getDadosLivro() {
+            System.out.println("Digite o nome do livro: ");
+            var nomeLivro = scanner.nextLine();
+            var json = consumo.obterDados(ENDERECO + nomeLivro.replace(" ", "+" + API_KEY));
+            DadosLivro dados = conversor.obterDados(json, DadosLivro.class);
+            return dados;
+      }
+
+}
--- src/main/java/br/com/pfc/biblioteca/model/Livro.java
+++ src/main/java/br/com/pfc/biblioteca/model/Livro.java
@@ -1,11 +1,85 @@
 package br.com.pfc.biblioteca.model;
 
+import br.com.pfc.biblioteca.dto.DadosLivro;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "livros")
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String titulo;
     private String autor;
     private int anoLancamento;
     private int numeroPagina;
-    private double avalliacao;
+    private double avaliacao;
+
+    public Livro(){}
+
+    public Livro(DadosLivro dadosLivro){
+        this.titulo=dadosLivro.titulo();
+        this.autor=dadosLivro.autor();
+        this.anoLancamento=dadosLivro.anoLancamento();
+        this.numeroPagina=dadosLivro.numeroPagina();
+        this.avaliacao=dadosLivro.avalliacao();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public int getNumeroPagina() {
+        return numeroPagina;
+    }
+
+    public void setNumeroPagina(int numeroPagina) {
+        this.numeroPagina = numeroPagina;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro = " + titulo + '\'' +
+                ", autor = " + autor + '\'' +
+                ", anoLancamento = " + anoLancamento +
+                ", numeroPagina = " + numeroPagina +
+                ", avaliacao = " + avaliacao;
+    }
 }
