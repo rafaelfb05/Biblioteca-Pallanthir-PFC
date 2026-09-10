046eb890172b51c1adb24fa1e9fa0e1716cff721
Felipe Barbosa Miranda <jcmcs312@gmail.com>
2026-09-10 08:47:34 -0300

    testando url

--- src/main/java/br/com/pfc/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/pfc/biblioteca/controller/LivroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("https://biblioteca-pallanthir-pfc.onrender.com")
+@RequestMapping("https://biblioteca-pallanthir-pfc.onrender.com/livros")
 public class LivroController {
 
     @Autowired
