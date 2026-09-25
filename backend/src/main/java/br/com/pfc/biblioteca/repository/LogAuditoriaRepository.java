c92f4d770d054fe1867560f1a7594189b4e27188
plugiin1 <jcmcs312@gmail.com>
2026-09-25 20:15:58 -0300

    fix(): Correção dá váriavel do MongoDB na application.properties

--- backend/src/main/java/br/com/pfc/biblioteca/repository/LogAuditoriaRepository.java
+++ backend/src/main/java/br/com/pfc/biblioteca/repository/LogAuditoriaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface LogAuditoriaRepository extends MongoRepository {
+public interface LogAuditoriaRepository extends MongoRepository <LogAuditoria, String> {
 
     List<LogAuditoria> findAllByOrderByDataHoraDesc();
 
