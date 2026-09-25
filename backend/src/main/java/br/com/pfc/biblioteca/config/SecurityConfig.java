036eff6dc4a88c7de25e0ce7d591fa942c78b8b7
plugiin1 <jcmcs312@gmail.com>
2026-09-25 20:49:39 -0300

    fix(): Spring Security barrando o Front

--- backend/src/main/java/br/com/pfc/biblioteca/config/SecurityConfig.java
+++ backend/src/main/java/br/com/pfc/biblioteca/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import br.com.pfc.biblioteca.infra.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,8 +23,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuarios/cadastro", "/usuarios/login",
